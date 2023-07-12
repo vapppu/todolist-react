4239cfd7f31f7a4cad583b455217518d23d5a0d4
vapppu <veera.hiltunen@gmail.com>
2023-07-12 19:23:15 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import TodoTable from './TodoTable';
 import './App.css';
 
 function App() {
+  const [todo, setTodo] = useState({description: '', date: ''});
+  const [todos, setTodos] = useState([]);
+
+  const inputChanged = (event) => {
+    setTodo({...todo, [event.target.name]: event.target.value});
+  }
+
+  const addTodo = () => {
+    setTodos([...todos, todo]);
+    setTodo({description: '', date: ''});
+  }
+
+  const deleteTodo = (row) => {
+    setTodos(todos.filter((todo, index) => index !== row));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input placeholder="Description" name="description" value={todo.description} onChange={inputChanged} />
+      <input placeholder="Date" name="date" value={todo.date} onChange={inputChanged}/>
+      <button onClick={addTodo}>Add</button>
+      <TodoTable todos={todos} deleteTodo={deleteTodo} />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import TodoTable from "./TodoTable";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import { fireEvent } from "@testing-library/react";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+
+test("renders todotable", () => {
+  const row = [{ description: "Go to coffee", date: "24.11.2022" }];
+  render(<TodoTable todos={row} />);
+
+  const tableRow = screen.getByText("Go to coffee");
+  expect(tableRow).toBeInTheDocument();
 });
+test('add todo',() => {
+  render(<App/>);
+
+  const desc = screen.getByPlaceholderText('Description');
+  fireEvent.change(desc, { target: { value: 'Go to coffee' } })
+  const date = screen.getByPlaceholderText('Date');
+  fireEvent.change(date, { target: { value: '29.11.2022' } })
+
+  fireEvent.click(screen.getByText('Add'));
+  expect(screen.getByText('Go to coffee')).toBeInTheDocument();
+})
